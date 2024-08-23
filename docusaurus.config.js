76b2b243791903c1f620fc75b6bf920e4a2bd837
tedthetrumpet <tedthetrumpet@gmail.com>
2024-08-23 15:02:29 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'tedthetrumpet', // Usually your GitHub org/user name.
   projectName: 'rcstest', // Usually your repo name.
+  trailingSlash: false,
 
 
 
